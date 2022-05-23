aef0b2deb01a5e9028700369cbe3b68a3907b428
Benson Firdy <bensonfirdy@gmail.com>
2022-05-22 22:48:48 -0400

    Pokemon

---
+++ React/pokemon/pokemon/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pokemon from './components/Pokemon';
+
+function App() {
+  return (
+    <div className="App">
+      What do you like to play? Pokemon!!!
+      <Pokemon></Pokemon>
+    </div>
+  );
+}
+
+export default App;
